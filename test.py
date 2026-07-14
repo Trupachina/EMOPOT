ea29bbba1fc42119fa445833adcf9675bb52c304
Trupachina <116503881+Trupachina@users.noreply.github.com>
2026-07-14 19:20:01 +0300

    Remove hardcoded API key from test.py
    Replace exposed Gemini API key with a placeholder instruction. Users should now provide their own API key to run the code securely.

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client = genai.Client(api_key="AIzaSyAC3ls1OdOMVvZGhMUVKgqfdoNc1YpDc9o")
+client = genai.Client(api_key="Your own *secret* Gemini/OpenAI/etc API key")
 
 response = client.models.generate_content(
     model="gemini-2.5-flash", contents="Напиши рабочий код калькулятора c визуальной оболойчкой на Python! Визуал современный, опирайся на дизайны Google, Apple и Яндекс, у окна должна быть возможность ммасштабирования без ошибок! Добавь возможность ввода текста с клавиатуры! Без приветственных фраз и описания, только чистый код с коментариями!"
